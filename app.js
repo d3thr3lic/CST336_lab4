0524d90827d2e87763ce3a45c10e5088dbd83ad8
EC2 Default User <ec2-user@ip-172-31-19-41.us-west-2.compute.internal>
2019-11-15 02:57:47 +0000

    asdf

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get("/earth", function(req, res){
     res.render("earth.html");
 });
 
+app.get("/mars", function(req, res){
+    res.render("mars.html");
+});
+
 //server listener
 //app.listen("8080","127.0.0.1", function(){
 app.listen(process.env.PORT, process.env.IP, function(){
